87ac41d336eb0a175ea7b8583c928763bd4dc53c
hamzah <hamzah.madni01@gmail.com>
2023-01-25 23:42:20 +0000

    scenario B is passing

--- app/Rates/Calculator.php
+++ app/Rates/Calculator.php
@@ -32,35 +32,72 @@ class Calculator implements CalculatorContract
         echo "\nNEW DATA SET\n\n\n\n\n";
         $timeCost = 0;
         $distanceCost = 0;
+        $minutesElapsed = 0;
+        $twentyFourHourTotal = 0;
+        $twentyFourHourTotals = collect();
         $fifteenMinuteChunks = collect(new DatePeriod($start, new DateInterval('PT15M'), $end));
 
-        $fifteenMinuteChunks->each(function ($chunk) use (&$timeCost) {
+        $fifteenMinuteChunks->each(function ($chunk) use (&$timeCost, &$minutesElapsed, &$twentyFourHourTotal, &$twentyFourHourTotals, $start, $end) {
+            $minutesElapsed += 15;
             echo "\n-------------------------\n";
             echo "\nCURRENT TIME: {$chunk->toTimeString()}\n";
             echo "\nTIME COST: {$timeCost}\n";
             if ($chunk->isWeekday()) {
                 $this->rates
                     ->filter(fn ($rate) => !$rate->is_weekend && $rate->rate_type === 'time')
-                    ->each(function ($rate) use ($chunk, &$timeCost) {
+                    ->each(function ($rate) use ($chunk, &$timeCost, &$twentyFourHourTotal) {
                         if ($chunk->hour >= $rate->start_hour && $chunk->hour < 24 || $chunk->hour <= $rate->end_hour) {
                             echo "\nRATE BEING ADDED: {$rate->default_rate}\n";
-                            $timeCost  = $timeCost  + $rate->default_rate;
+                            $timeCost = $timeCost  + $rate->default_rate;
+                            $twentyFourHourTotal += $rate->default_rate;
                             echo "\nTIME COST AFTER: {$timeCost}\n";
                         }
                     });
             } else {
                 $this->rates
                     ->filter(fn ($rate) => $rate->is_weekend && $rate->rate_type === 'time')
-                    ->each(function ($rate) use ($chunk, &$timeCost) {
+                    ->each(function ($rate) use ($chunk, &$timeCost, &$twentyFourHourTotal) {
                         if ($chunk->hour >= $rate->start_hour && $chunk->hour < 24 || $chunk->hour <= $rate->end_hour) {
                             echo "\nRATE BEING ADDED: {$rate->default_rate}\n";
-                            $timeCost  = $timeCost  + $rate->default_rate;
+                            $timeCost = $timeCost  + $rate->default_rate;
+                            $twentyFourHourTotal += $rate->default_rate;
                             echo "\nTIME COST AFTER: {$timeCost}\n";
                         }
                     });
             }
+
+            if ($minutesElapsed === min($start->diffInMinutes($end), 1440)) {
+                echo "\nMINUTES ELAPSED: {$minutesElapsed}\n";
+                $twentyFourHourTotals->push($twentyFourHourTotal);
+                $twentyFourHourTotal = 0;
+                $minutesElapsed = 0;
+            }
         });
 
-        return new Result($timeCost, new Distance(0));
+        $dailyMaxRate = $this->rates->filter(fn ($rate) => $rate->daily_max)->first();
+        if ($dailyMaxRate) {
+            echo "\nAPPLYING DAILY MAX\n";
+            $totalOverChargedAmount = 0;
+            $twentyFourHourTotals->each(function ($total) use (&$totalOverChargedAmount, $dailyMaxRate) {
+                if ($total > $dailyMaxRate->default_rate) {
+                    $totalOverChargedAmount += $total - $dailyMaxRate->default_rate;
+                }
+            });
+
+            echo "\nTIME COST BEFORE: {$timeCost}\n";
+            $timeCost -= $totalOverChargedAmount;
+            echo "\nTIME COST AFTER: {$timeCost}\n";
+        }
+
+        $distanceRate = $this->rates->filter(fn ($rate) => $rate->rate_type === 'distance')->first();
+        if (isset($distanceRate->special_rate)) {
+            $distanceToBeChargedAtDefaultRate = $distance - $distanceRate->special_rate_limit;
+
+            $distanceCost = $distanceToBeChargedAtDefaultRate > 0
+                ? $distanceRate->special_rate * $distanceRate->special_rate_limit + $distanceRate->default_rate * $distanceToBeChargedAtDefaultRate
+                : $distanceRate->special_rate * $distance;
+        }
+
+        return new Result($timeCost, new Distance($distanceCost));
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -8,6 +8,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // Company A
+        // 400/hr weekdays
         Rate::create([
             'company_name' => 'A',
             'rate_type' => 'time',
@@ -15,6 +16,7 @@ class DatabaseSeeder extends Seeder
             'end_hour' => 24,
             'default_rate' => 100,
         ]);
+        // 400/hr weekends
         Rate::create([
             'company_name' => 'A',
             'rate_type' => 'time',
@@ -23,10 +25,45 @@ class DatabaseSeeder extends Seeder
             'default_rate' => 100,
             'is_weekend' => true,
         ]);
+        // 15/mile
         Rate::create([
             'company_name' => 'A',
             'rate_type' => 'distance',
             'default_rate' => 15,
+        ]);
+
+        // Company B
+        // 1500/hr weekdays
+        Rate::create([
+            'company_name' => 'B',
+            'rate_type' => 'time',
+            'start_hour' => 0,
+            'end_hour' => 24,
+            'default_rate' => 375,
+        ]);
+        // 1500/hr weekends
+        Rate::create([
+            'company_name' => 'B',
+            'rate_type' => 'time',
+            'start_hour' => 0,
+            'end_hour' => 24,
+            'default_rate' => 375,
+            'is_weekend' => true,
+        ]);
+        // 8500 max per 24hr
+        Rate::create([
+            'company_name' => 'B',
+            'rate_type' => 'max',
+            'default_rate' => 8500,
+            'daily_max' => true,
+        ]);
+        // 50/km first 50 free
+        Rate::create([
+            'company_name' => 'B',
+            'rate_type' => 'distance',
+            'default_rate' => 50,
+            'special_rate' => 0,
+            'special_rate_limit' => 50,
         ]);
     }
 }
